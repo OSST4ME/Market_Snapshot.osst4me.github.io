7a651a5bec79a352478dd573b9aa64414d25bea4
corbin.felsheim@gmail.com <corbinfelsheim@Corbins-MacBook-Pro.local>
2019-03-25 21:33:44 -0700

    pushing commit

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,102 @@
 var db = require("../models");
-
+var availableZipsArray = [
+  "Allied Gardens-92120",
+    "Alpine-91901",
+    "Bay Park-92110",
+    "Bonita-91902",
+    "Bonsall-92003",
+    "Borrego Springs-92004",
+    "Boulevard-91905",
+    "Campo-91906",
+    "Cardiff-by-the-Sea-92007",
+    "Carlsbad-92008",
+    "Carlsbad-92009",
+    "Carlsbad-92010",
+    "Carlsbad-92011",
+    "Carmel Valley-92130",
+    "Chula Vista-91910",
+    "Chula Vista-91911",
+    "Chula Vista-91913",
+    "Chula Vista-91914",
+    "Chula Vista-91915",
+    "Clairemont-92117",
+    "College Grove-92115",
+    "Coronado-92118",
+    "Del Mar-92014",
+    "Descanso-91916",
+    "Downtown-92101",
+    "Dulzura-91917",
+    "East San Diego-92102",
+    "East San Diego-92105",
+    "El Cajon-92019",
+    "El Cajon-92020",
+    "El Cajon-92021",
+    "Encanto-92114",
+    "Encinitas-92024",
+    "Escondido-92025",
+    "Escondido-92026",
+    "Escondido-92027",
+    "Escondido-92029",
+    "Fallbrook-92028",
+    "Hillcrest-92103",
+    "Imperial Beach-91932",
+    "Jacumba-91934",
+    "Jamul-91935",
+    "Julian-92036",
+    "La Jolla-92037",
+    "La Mesa-91941",
+    "La Mesa-91942",
+    "Lakeside-92040",
+    "Lemon Grove-91945",
+    "Linda Vista-92111",
+    "Logan Heights-92113",
+    "Mira Mesa-92126",
+    "Mission Village-92123",
+    "National City-91950",
+    "Normal Heights-92116",
+    "North Park-92104",
+    "Ocean Beach-92107",
+    "Oceanside-92054",
+    "Oceanside-92056",
+    "Oceanside-92057",
+    "Oceanside-92058",
+    "Pacific Beach-92109",
+    "Palomar Mountain-92060",
+    "Paradise Hills-92139",
+    "Pauma Valley-92061",
+    "Pine Valley-91962",
+    "Point Loma-92106",
+    "Potrero-91963",
+    "Poway-92064",
+    "Ramona-92065",
+    "Ranch Bernardo-92127",
+    "Ranch Bernardo-92128",
+    "Ranch Penasquitos-92129",
+    "Ranch Santa Fe-92067",
+    "Ranch Santa Fe-92091",
+    "Ranchita-92066",
+    "San Carlos-92119",
+    "San Diego-92108",
+    "San Diego-92112",
+    "San Marcos-92069",
+    "San Marcos-92078",
+    "San Ysidro-92173",
+    "Santa Ysabel-92070",
+    "Santee-92071",
+    "Scripps Ranch-92131",
+    "Solana Beach-92075",
+    "Sorrento Valley-92121",
+    "South San Diego-92154",
+    "Spring Valley-91977",
+    "Spring Valley-91978",
+    "Tierrasanta-92124",
+    "University City-92122",
+    "Valley Center-92082",
+    "Vista-92081",
+    "Vista-92083",
+    "Vista-92084",
+    "Warner Springs-92086"
+]
 module.exports = function (app) {
   // Load index page
   app.get("/", function (req, res) {
@@ -10,8 +107,8 @@ module.exports = function (app) {
       include: [db.Comment]
     }).then(function (dbReturn) {
       var dbLength = dbReturn.length - 1;
-      res.json(dbReturn)
-      var availableZipsArray = [];
+      // res.json(dbReturn)
+
       var chartMonths = [];
       var houseChartMedian = [];
       var houseChartSales = [];
@@ -27,10 +124,10 @@ module.exports = function (app) {
         //get all the comments
 
       }
-      for (i = 0; i < 2; i++) {
-        availableZipsArray.push(dbReturn[i].zipConcat)
-      }
-      console.log(availableZipsArray)
+      // for (i = 0; i < 2; i++) {
+      //   availableZipsArray.push(dbReturn[i].zipConcat)
+      // }
+      
       res.render("index", {
         availableZips: availableZipsArray,
         houseSales: dbReturn[dbLength].houseUnits,
@@ -54,8 +151,9 @@ module.exports = function (app) {
         zipConcat: req.params.zip
       }
     }).then(function (dbReturn) {
-      console.log("zip code return array returned: " + dbReturn)
+      
       var dbLength = dbReturn.length - 1;
+      console.log("db length -1 is" + dbLength)
       var chartMonths = [];
       var houseChartMedian = [];
       var houseChartSales = [];
@@ -69,9 +167,10 @@ module.exports = function (app) {
         condoChartMedian.push(dbReturn[i].condoPrice);
         condoChartSales.push(dbReturn[i].condoUnits)
       }
-      console.log("Our return lenghts latest month is at position " + dbLength)
-      res.render("example", {
+      console.log("Our return lengths latest month is at position " + dbLength)
+      res.render("index", {
         //all the data for the green headers
+        availableZips: availableZipsArray,
         houseSales: dbReturn[dbLength].houseUnits,
         medianHouse: dbReturn[dbLength].housePrice,
         houseYear: dbReturn[dbLength].houseChange,
@@ -93,7 +192,7 @@ module.exports = function (app) {
       Comment_Text: req.body.comment,
       Email: req.body.email
     }).then(function (dbReturn) {
-      res.json(dbReturn)
+      res.redirect("/housing/:zip")
     })
   })
 
